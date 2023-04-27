981eb5a3df24ecb20b1bd3bc202cae677e0fb64d
Jaegerly <teamcocohead@gmail.com>
2023-04-27 08:18:04 -0400

    update comments for store

--- models/store.js
+++ models/store.js
@@ -1,6 +1,10 @@
-/* a store for '__dirname' that can be accessed anywhere in program. */
-/* this object prevents './router.js' from having to recalculate '__dirname'. */
+/* a store that can be accessed anywhere in program. */
+
+/* in particular, this object has two properties: */
+/* 1. '__dirname': prevents '../controllers/router.js' from having to recalculate '__dirname'. */
+/* 2. 'states': holds merged data from './states.json' and remote database. */
 
 export const store = {
-    '__dirname': null
+    '__dirname': null,
+    'states': null
 }
